3a80ffd5ec51ff42d7b36010cecc5d00099efccc
Sneh <snehkoul1999@gmail.com>
2018-07-08 10:14:18 -0400

    Messaging using Node.js

---
+++ server.js
@@ -0,0 +1,24 @@
+ var express = require('express')
+
+ var app = express()
+
+ app.use(express.static(__dirname));
+
+ var messages = [
+     {
+         name:'Tim',
+         message:'Hi'
+     },
+     {
+        name:'Jane',
+        message:'Hello'
+    }
+ ]
+
+ app.get('/messages',(req,res)=>{
+     res.send(messages);
+ })
+ app.listen(3000,() =>{
+     console.log('server is listening on port 3000')
+ }); 
+
