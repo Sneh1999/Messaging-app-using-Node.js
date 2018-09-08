6775c3c9e13179de42cb6cd1d20786877bcd50b6
Sneh <snehkoul1999@gmail.com>
2018-09-08 16:09:52 -0400

    Two-clients-communicating

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
  var express = require('express')
-
+ var bodyParser = require('body-parser')
  var app = express()
 
  app.use(express.static(__dirname)); //becasue we are using static files
-
+ app.use(bodyParser.json())
+ app.use(bodyParser.urlencoded({extended:false})) //data from the browswer comes in urlencoded format
  var messages = [
      {
          name:'Tim',
@@ -19,6 +20,10 @@
  app.get('/messages',(req,res)=>{
      res.send(messages);
  })
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200);
+})
  app.listen(3000,() =>{
      console.log('server is listening on port 3000')
  }); 
